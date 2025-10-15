72df4b43c6821ef8840a00bbe9b7093ccbec2cbe
Xonzoda Rajabboyeva <xonzodabegim0810@gmail.com>
2025-10-15 21:19:01 +0500

    The 'FizzBuzz' challenge

---
+++ fizzbuzz_challenge_p4.py
@@ -0,0 +1,10 @@
+print("--- FizzBuzz Challenge (1-100) ---")
+for i in range(1, 101):
+    if not i % 15:
+        print("FizzBuzz")
+    elif not i % 3:
+        print("Fizz")
+    elif not i % 5:
+        print("Buzz")
+    else:
+        print(i)
